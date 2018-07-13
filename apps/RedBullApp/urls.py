b5adcd6500b2c3465c1cd2fba3e6afead9c2f42e
Paul <paulwinegard@gmail.com>
2018-07-13 17:00:14 -0400

    Subsequent routes connected and views generated.  Linked to templates

--- apps/RedBullApp/urls.py
+++ apps/RedBullApp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views 
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^form_input$', views.form_input),
+    url(r'^confirmation$', views.confirmation),
 ]
--- apps/RedBullApp/views.py
+++ apps/RedBullApp/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def form_input(request):
+    return render(request, 'form_input.html')
+
+def confirmation(request):
+    return render(request, 'confirmation.html')
