b3ca9596d81cc1ba0cfc4039ed685e2907ffe79e
Paul <paulwinegard@gmail.com>
2018-07-14 20:55:03 -0400

    Models generated.  Forms create users.  Logic complete.  Admin portal created and formatted.  Admin filter and searcg functions added. New Templates and routes generated to display users seperated by waitlist and current attendess

--- apps/RedBullApp/models.py
+++ apps/RedBullApp/models.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    wristband = models.BooleanField()
+    invitation = models.CharField(max_length=255,null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+       return self.first_name + " " + self.last_name
+
 # Create your models here.
--- apps/RedBullApp/views.py
+++ apps/RedBullApp/views.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def form_input(request):
-    return render(request, 'form_input.html')
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email = request.POST['email']
+    invitation = ""
+    if 'invitation' in request.POST:
+        invitation = request.POST['invitation']
+    wristband = False
+    if 'yes' in request.POST:
+        wristband = True
+    print(wristband)
+    User.objects.create(first_name=first_name, last_name=last_name, email=email, wristband=wristband, invitation=invitation)
+    return redirect("/display_users")
+
+def display_users(request):
+    users = User.objects.filter(wristband=True)
+    context = {
+        'users' : users
+    }
+    return render(request, "display_users.html", context)
+def waitlist(request):
+    users = User.objects.filter(wristband=False)
+    context = {
+        'users' : users
+    }
+    return render(request, "waitlist.html", context)
 
 def confirmation(request):
     return render(request, 'confirmation.html')
