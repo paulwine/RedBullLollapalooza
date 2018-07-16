4842b50b34c017f23e5c66fdd65f5cb8b9e95250
Will Chatterson <wjchat@umich.edu>
2018-07-16 13:02:10 -0400

    complete windows style

--- apps/RedBullApp/views.py
+++ apps/RedBullApp/views.py
@@ -6,7 +6,7 @@ from .models import User
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-
+ 
 
 # Create your views here.
 def index(request):
