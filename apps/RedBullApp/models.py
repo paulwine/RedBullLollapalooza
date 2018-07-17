7f27f276bad41c6be30d82552f96720457e1a218
Paul <paulwinegard@gmail.com>
2018-07-16 23:43:48 -0400

    Final Product.  Email images linked.  Portal Display added, search functionality added.  Dope sauce

--- apps/RedBullApp/models.py
+++ apps/RedBullApp/models.py
@@ -9,10 +9,12 @@ class User(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     wristband = models.BooleanField()
+    phone = models.CharField(max_length=255)
     invitation = models.CharField(max_length=255,null=True, blank=True)
-
+    affiliation = models.CharField(max_length=255,null=True, blank=True)
+    referall = models.CharField(max_length=255,null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __unicode__(self):
        return self.first_name + " " + self.last_name
 
--- apps/RedBullApp/views.py
+++ apps/RedBullApp/views.py
@@ -16,16 +16,23 @@ def form_input(request):
     first_name = request.POST['first_name']
     last_name = request.POST['last_name']
     email = request.POST['email']
+    phone = request.POST['phone']
     invitation = ""
+    referall = ""
+    affiliation = ""
     if 'invitation' in request.POST:
         invitation = request.POST['invitation']
+    if 'referall' in request.POST:
+        referall = request.POST['referall']
+    if 'affiliation' in request.POST:
+        affiliation = request.POST['affiliation']
     wristband = False
     message_html = render_to_string('waitlist_email.html')
     if 'yes' in request.POST:
         wristband = True
         message_html = render_to_string('email.html')
     print(wristband)
-    User.objects.create(first_name=first_name, last_name=last_name, email=email, wristband=wristband, invitation=invitation)
+    User.objects.create(first_name=first_name, last_name=last_name, phone=phone,email=email, wristband=wristband, affiliation=affiliation, referall=referall,invitation=invitation)
     
     
     send_mail(
@@ -36,15 +43,60 @@ def form_input(request):
     fail_silently=False,
     html_message = message_html,
 )
-    return redirect("/display_users")
+    return redirect("/confirmation")
 
 def display_users(request):
+    if 'current_admin' not in request.session:
+        return redirect("/")
     users = User.objects.filter(wristband=True)
     context = {
         'users' : users
     }
     return render(request, "display_users.html", context)
+def display_users_filtered(request):
+    if 'current_admin' not in request.session:
+        return redirect("/")
+    name_filter = ""
+    invite_filter = ""
+    users = User.objects.filter(wristband=True)
+    if request.POST['last_name'] != "":
+        name_filter = request.POST["last_name"]
+        users = User.objects.filter(wristband=True, last_name=name_filter)
+    if request.POST['invitation'] != "":
+        invite_filter = request.POST["invitation"]
+        users = User.objects.filter(wristband=True, invitation=invite_filter)
+    if request.POST['invitation'] != "" and request.POST['last_name'] != "":
+        name_filter = request.POST["last_name"]
+        invite_filter = request.POST["invitation"]
+        users = User.objects.filter(wristband=True, invitation=invite_filter, last_name=name_filter)
+    context = {
+        'users' : users
+    }
+    return render(request, "display_users.html", context)
 def waitlist(request):
+    if 'current_admin' not in request.session:
+        return redirect("/")
+    users = User.objects.filter(wristband=False)
+    context = {
+        'users' : users
+    }
+    return render(request, "waitlist.html", context)
+def waitlist_filtered(request):
+    if 'current_admin' not in request.session:
+        return redirect("/")
+    name_filter = ""
+    invite_filter = ""
+    users = User.objects.filter(wristband=False)
+    if request.POST['last_name'] != "":
+        name_filter = request.POST["last_name"]
+        users = User.objects.filter(wristband=False, last_name=name_filter)
+    if request.POST['invitation'] != "":
+        invite_filter = request.POST["invitation"]
+        users = User.objects.filter(wristband=False, invitation=invite_filter)
+    if request.POST['invitation'] != "" and request.POST['last_name'] != "":
+        name_filter = request.POST["last_name"]
+        invite_filter = request.POST["invitation"]
+        users = User.objects.filter(wristband=True, invitation=invite_filter, last_name=name_filter)
     users = User.objects.filter(wristband=False)
     context = {
         'users' : users
@@ -52,4 +104,16 @@ def waitlist(request):
     return render(request, "waitlist.html", context)
 
 def confirmation(request):
+
     return render(request, 'confirmation.html')
+
+def portal(request):
+    return render(request, 'portal.html')
+
+def portal_login(request):
+    if request.method == 'GET':
+        return redirect("/portal")
+    if request.POST['username'] != 'RedBullAdmin' or request.POST['password'] != 'secretkey15':
+        return redirect("/portal")
+    request.session["current_admin"] = "MIKE JONES"
+    return render(request, 'portal_login.html')
