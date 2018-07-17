2832ef095d93627df7f6641fe3ed5ac1eab8acbb
Paul <paulwinegard@gmail.com>
2018-07-16 23:43:48 -0400

    Final Product.  Email images linked.  Portal Display added, search functionality added.  Dope sauce

--- apps/RedBullApp/urls.py
+++ apps/RedBullApp/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     url(r'^confirmation$', views.confirmation),
     url(r'^display_users$', views.display_users),
     url(r'^waitlist$', views.waitlist),
+    url(r'^display_users_filtered$', views.display_users_filtered),
+    url(r'^waitlist_filtered$', views.waitlist_filtered),
+    url(r'^portal$', views.portal),
+    url(r'^portal_login$', views.portal_login),
+
 ]
