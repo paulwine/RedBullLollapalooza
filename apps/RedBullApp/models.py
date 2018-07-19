cb7de8ba0cc19a58510c3d4600a9ef1d964d0bb6
Paul <paulwinegard@gmail.com>
2018-07-18 20:06:37 -0400

    Email setup to run asyc.  Settings edited to go live

--- apps/RedBullApp/models.py
+++ apps/RedBullApp/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.mail import EmailMessage
+import threading
 
 
 class User(models.Model):
@@ -17,5 +19,20 @@ class User(models.Model):
 
     def __unicode__(self):
        return self.first_name + " " + self.last_name
+
+class EmailThread(threading.Thread):
+    def __init__(self, subject, html_content, recipient_list, sender):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.html_content = html_content
+        self.sender = sender
+        threading.Thread.__init__(self)
+
+    def run(self):
+        msg = EmailMessage(self.subject, self.html_content, self.sender, self.recipient_list)
+        msg.content_subtype = 'html'
+        msg.send()
+
+
 
 # Create your models here.
